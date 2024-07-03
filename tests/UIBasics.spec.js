c23ae672b3dd841e9e7874687b07caf75f5ed09a
sharma <sharma@DESKTOP-IS2799H>
2024-07-03 16:15:56 +0530

    files formatted

--- tests/UIBasics.spec.js
+++ tests/UIBasics.spec.js
@@ -1,21 +1,19 @@
-import { test,expect } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
 test(
-    ' Browser Context Basic UI Tests',  async ({browser})=>
-    {
+    ' Browser Context Basic UI Tests', async ({ browser }) => {
 
-        const context = await browser.newContext();
-        const page = await context.newPage();
-        await page.goto('https://playwright.dev/');
-        console.log(await page.title());
-    });
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.goto('https://playwright.dev/');
+    console.log(await page.title());
+});
 
-    test(
-        'Page Basic UI Tests2',  async ({page})=>
-        {
-            await page.goto('https://google.com/');
-            console.log(await page.title());
-            expect( await page.title()).toBe("Google");
-            
-            
-        });
+test(
+    'Page Basic UI Tests2', async ({ page }) => {
+    await page.goto('https://google.com/');
+    console.log(await page.title());
+    expect(await page.title()).toBe("Google");
+
+
+});
