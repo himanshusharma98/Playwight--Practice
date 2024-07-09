e9087cbb141cbfa59bf9c74fe3ea81af34e73d5e
sharma <sharma@DESKTOP-IS2799H>
2024-07-09 12:10:42 +0530

    removed  extra space

--- tests/SpecialLocators.spec.js
+++ tests/SpecialLocators.spec.js
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
- 
+
 test('Playwright Special Locators', async ({ page }) => {
-  
+
     await page.goto("https://rahulshettyacademy.com/angularpractice/");
     await page.getByLabel('Check me out if you Love IceCreams!').click();
     await page.getByLabel('Employed').check();
@@ -10,6 +10,6 @@ test('Playwright Special Locators', async ({ page }) => {
     await page.getByRole('button', { name: 'Submit' }).click();
     await page.getByText('Success! The Form has been submitted successfully!').isVisible();
     await page.getByRole('link', { name: 'Shop' }).click();
-    
+
 
 })
