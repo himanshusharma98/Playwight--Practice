be28180273bc4ade82e6a6d7decdd7596bb282d1
sharma <sharma@DESKTOP-IS2799H>
2024-07-09 12:06:03 +0530

    Special Locators Files Added

---
+++ tests/SpecialLocators.spec.js
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test';
+ 
+test('Playwright Special Locators', async ({ page }) => {
+  
+    await page.goto("https://rahulshettyacademy.com/angularpractice/");
+    await page.getByLabel('Check me out if you Love IceCreams!').click();
+    await page.getByLabel('Employed').check();
+    await page.getByLabel('Gender').selectOption('Male');
+    await page.getByPlaceholder('Password').fill('12345');
+    await page.getByRole('button', { name: 'Submit' }).click();
+    await page.getByText('Success! The Form has been submitted successfully!').isVisible();
+    await page.getByRole('link', { name: 'Shop' }).click();
+    
+
+})
