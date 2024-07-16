53db83282d4d16191f6b2fa4741bed17210c78d7
sharma <sharma@DESKTOP-IS2799H>
2024-07-16 18:17:53 +0530

    Visual testing & Screenshots

--- tests/MoreValidations.spec.js
+++ tests/MoreValidations.spec.js
@@ -14,3 +14,18 @@ test('More Validations', async ({ page }) => {
     console.log(textCheck.split(' ')[1]);
 
 });
+
+test('Screenshot & Visual Comparison', async ({ page }) => {
+    await page.goto('https://rahulshettyacademy.com/AutomationPractice/');
+    expect(await page.locator('#displayed-text')).toBeVisible();
+    await page.locator('#displayed-text').screenshot({ path: 'partial_screenshot.png'});
+    await page.locator('#hide-textbox').click();
+    await page.screenshot({ path: 'screenshot.png', fullPage: true});
+    expect( page.locator('#displayed-text')).toBeHidden();
+});
+
+test.only(' Visual Testing', async ({ page }) => {
+    await page.goto('https://www.google.com/');
+    expect(await page.screenshot()).toMatchSnapshot('home.png');
+    
+});
